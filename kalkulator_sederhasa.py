c855907893f1f4fc8584dc59cb98d37be2974c60
Edgar Rizki <hutaoo@LAPTOP-9BIQAQVN.localdomain>
2026-08-23 20:49:29 +0700

    nambah fitur looping ajah

--- kalkulator_sederhasa.py
+++ kalkulator_sederhasa.py
@@ -7,22 +7,24 @@ print("2. Pengurangan")
 print("3. Perkalian")
 print("4. Pembagian")
 input_user = int(input("Pilih dari 1-4: "))
+s = "y"
 
 if input_user < 5:
     while True:
-        a = (int(input("Masukan angka: ")))
-        b = (int(input("Masukan angka: ")))
-        if input_user == 1:
-            print(f"Hasil dari {a} + {b} = {a + b}")
+        if s == "y":
+            a = (int(input("Masukan angka: ")))
+            b = (int(input("Masukan angka: ")))
+            if input_user == 1:
+                print(f"Hasil dari {a} + {b} = {a + b}")
+            elif input_user == 2:
+                print(f"Hasil dari {a} - {b} = {a - b}")
+            elif input_user == 3:
+                print(f"Hasil dari {a} x {b} = {a * b}")
+            elif input_user == 4:
+                print(f"Hasil dari {a} : {b} = {a / b}")
+        elif s == "n":
+            print("program berhenti") 
             break
-        elif input_user == 2:
-            print(f"Hasil dari {a} - {b} = {a - b}")
-            break
-        elif input_user == 3:
-            print(f"Hasil dari {a} x {b} = {a * b}")
-            break
-        elif input_user == 4:
-            print(f"Hasil dari {a} : {b} = {a / b}")
-            break
+        s = str(input("Apakah ingin lanjut? y/n: "))
 else:
     print("jangan masukan angka selain di atas")
