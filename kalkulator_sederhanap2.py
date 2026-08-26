b99cc3e9231b8c153fd1d6891f9dfa073b49a878
Edgar Rizki <hutaoo@LAPTOP-9BIQAQVN.localdomain>
2026-08-26 11:48:05 +0700

    update

--- kalkulator_sederhanap2.py
+++ kalkulator_sederhanap2.py
@@ -2,13 +2,13 @@ import os
 
 def aritmatika(a, b, operasi_hitung):
     if operasi_hitung == "+":
-        print(f"hasil dari {a} + {b} = {a+ b}")
+        print(f"hasil dari {a:g} + {b:g} = {a+ b :g}")
     elif operasi_hitung == "-":
-        print(f"hasil dari {a} - {b} = {a- b}")
+        print(f"hasil dari {a:g} - {b:g} = {a- b :g}")
     elif operasi_hitung == "*":
-        print(f"hasil dari {a} * {b} = {a* b}")
+        print(f"hasil dari {a:g} * {b:g} = {a* b :g}")
     elif operasi_hitung == "/":
-        print(f"hasil dari {a} / {b} = {a/ b}")
+        print(f"hasil dari {a:g} / {b:g} = {a/ b :g}")
     else:
         raise ValueError("Simbol operasi tidak dikenal!")
 
@@ -16,18 +16,20 @@ s = "y"
 
 try:
     while True:
-        os.system('clear')
+        os.system("clear" or "cls")
         print("---------------------------------------------")
         print("            kalkulator sederhana            ")
         print("---------------------------------------------")
         if s == "y":
-            input1 = int(input("masukan angka Pertama: "))
-            operator = (input("masukan operasi hitung (+,-,*,/): "))
-            input2 = int(input("masukan angka Kedua: "))
+            input1 = float(input("masukan angka Pertama: "))
+            operator = str(input("masukan operasi hitung (+,-,*,/): "))
+            input2 = float(input("masukan angka Kedua: "))
 
-            hasil = aritmatika(input1, input2, operator)
+            print(aritmatika(input1, input2, operator))
         elif s == "n":
             print("keluar dari program")
+            break
+        else:
             break
         
         s = str(input("apakah mau lagi? y/n  ")) 
@@ -36,7 +38,8 @@ except ValueError as e:
     if "Simbol" in str(e):
         print(f"Error: {e}")
     else:
-        print("Error: Masukan angka yang benar")
+        print("Error: Masukan angka yang benar!")
+       
 
 except ZeroDivisionError:
-    print("Error: angka tidak bisa di bagi dengan nol")
+    print("Error: angka tidak bisa di bagi dengan nol!")
